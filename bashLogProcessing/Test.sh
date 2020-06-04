d26fb75f660ef3fb5b07231560f88f26554100d3
oldguardmd <oldguard@msn.com>
2020-06-04 10:29:01 -0400

    More examples

--- bashLogProcessing/Test.sh
+++ bashLogProcessing/Test.sh
@@ -3,10 +3,20 @@
 function get-Serialandtar (){
     printf "Enter the serial number to find memory errors for:\n"
     read serialNumber
-    printf "Enter the tar file full name with path:"
+    printf "Enter the tar file full name with path:\n"
     read tarFileName
 }
 
+function print-tar (){
+   printf "TAR File Location: ${tarFileName}\n" 
+
+}
+
+function print-serial (){
+   printf "Serial Number: ${serialNumber}\n" 
+
+}
+
 get-Serialandtar
-printf "TAR File Location: ${tarFileName}"
-printf "Serial Number: ${serialNumber}"
+print-serial
+print-tar
