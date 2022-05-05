c2f3ab47d842a7ced0d5f4b88c19eed784d510d1
oldguardmd <oldguard@msn.com>
2022-05-05 18:34:01 -0400

    Prototype - Fix issue with version matching - Updated Help

--- memoryDiag/set-diagnostic.ps1
+++ memoryDiag/set-diagnostic.ps1
@@ -11,6 +11,71 @@ reserved. Unless required by applicable law or agreed to separately in
 writing, software distributed under the License is distributed on an "AS
 IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.
+
+.DESCRIPTION
+This script is intended for new environments to test memory on the servers and generate processor / memory utilization 
+for the purposes of stress testing the server prior to production deployment. 
+
+This script was written for the UCS Mini, but should work with any UCS Manager based deployment. 
+
+Because the script makes changes, you should fully review how this script works before running it on your network. If you
+have production servers this script can cause harm to running production environments. This script will trigger a reboot
+of any servers it is directed towards, and by default that means blades 1 through 8 in chassis 1. If you run this with
+default settings, you can expect blades 1 through 8 to go down immidiately to run a memory test. It also creates or over 
+writes a diag policy called longMemoryTest as part of this process. 
+
+We trigger two tests in this script by default.
+
+    100 Passes of butterfly memory testing
+    100 Passes of prbs-killer memory testing
+
+For the average M5 server, you can expect this to run 12 to 24 hours depending on the amount of memory and the speed of the processor.
+
+.PARAMETER DomainList
+The domain list is a list of domains to be processed. All domains must have the same logon and password for them to be run 
+by the script at the same time. Multiple domains can be included by using commas to seperate the domain names or IPs. 
+Do not include spaces in the seperated values. 
+
+.PARAMETER failSafe
+The script will not run unless you use this switch. This is intended to prevent accidental trigger of the script.
+
+.PARAMETER butterFlyCount
+Set the number of passes the butterfly test should be completed.
+
+.PARAMETER prbskillerCount
+Set the number of passes the prbs-killer test should be completed.
+
+.PARAMETER Global:testName
+Set the name of the memory test policy. By default this is longMemoryTest
+
+.PARAMETER ServerList
+This array lists all of the servers to be rebooted (immediately) for memory testing. It is critical that you review this list
+before running this script. By default we reboot blades 1 through 8 on chassis 1. The two common formats for servers are:
+
+    Blade 1 on Chassis 1 as an example of a blade 
+    sys/chassis-1/blade-1
+
+    Rack mount Server 1 as an example
+    sys/rack-unit-1
+
+.EXAMPLE
+./set-diagnostic.ps1 -DomainList 1.1.1.10 -failsafe
+
+Running against a single domain using default settings. This will create a policy called longMemoryTest and then reboot
+all eight blade servers in chassis 1 if they exist.
+
+.EXAMPLE
+./set-diagnostic.ps1 -DomainList 1.1.1.10 -failsafe -prbskillerCount 2 -butterFlyCount 2
+
+Creates a policy called longMemoryTest on the 1.1.1.10 fabric interconnect domain with loop counts of 2 for the
+butterfly and PRBS-Killer tests.
+
+.EXAMPLE
+./set-diagnostic.ps1 -DomainList 1.1.1.10,1.1.2.10 -failsafe
+
+Creates policies in two different domains and reboots all 8 servers in both domains to perform the memory test. 
+
+
 #>
 [cmdletbinding()]
 param(
@@ -22,7 +87,13 @@ param(
     [parameter(mandatory=$false)]      [int]$prbskillerCount=100,
     [parameter(mandatory=$false)]      [array]$serverList = @(
         "sys/chassis-1/blade-1",
-        "sys/chassis-1/blade-2") 
+        "sys/chassis-1/blade-2",
+        "sys/chassis-1/blade-3",
+        "sys/chassis-1/blade-4",
+        "sys/chassis-1/blade-5",
+        "sys/chassis-1/blade-6",
+        "sys/chassis-1/blade-7",
+        "sys/chassis-1/blade-8") 
 )
 
 if ($failSafe -eq $false){
@@ -61,6 +132,8 @@ function write-screen {
     }    
 }
 function create-DiagPolicy(){
+    write-verbose "Writting Butterfly count of $butterFlyCount"
+    write-verbose "Writting prbs-killer count of $prbsKillerCount"
     try{
         $memTest = Get-UCSOrg root | Add-UcsManagedObject -ClassId diagRunPolicy -propertyMap @{Name = "$($Global:testName)"}
         $memTest_1 = $memTest | add-UcsManagedObject -class diagMemoryTest -propertyMap @{cpuFilter="all-cpus"; id="1"; loopCount="$butterFlyCount";  memChunkSize="big-chunk"; memSize="all"; order="1"; pattern="butterfly"; rn="test-1"; type="pmem2"}
@@ -73,26 +146,33 @@ function create-DiagPolicy(){
 
 function check-DiagPolicyValues($objExisting){
     #We should get three objects here, or we do not have the right number of policies. 
-    if ($objExisting.count -ne 3){
+    if ($objExisting.count -eq 3){
+        write-screen -type "INFO" -message "`tCorrect number of objects found in existing policy. Checking settings"
+    }
+    else{
+        write-verbose "`tWe should have 3 objects, but we returned $($objExisting.count)"
+        write-verbose "$($objExisting)"
         write-screen -type "WARN" -message "`tPolicy is not configured properly"
         return $false
     }
     #We should match all of these parameters on the first memory test policy
-    If ($policyCheck[1].LoopCount    -ne "$butterFlyCount" -or `
+    If ($policyCheck[1].LoopCount    -ne "$($butterFlyCount)" -or `
         $policyCheck[1].pattern      -ne "butterfly" -or `
         $policyCheck[1].CpuFilter    -ne "all-cpus" -or `
         $policyCheck[1].MemChunkSize -ne "big-chunk" -or `
         $policyCheck[1].MemSize      -ne "All"
     ){
+        write-screen -type "WARN" -message "`tButterfly test has a different settings. Recreating the policy"
         return $false
     }
     #We shoudl match all of these parameters on the second memory test policy
-    If ($policyCheck[2].LoopCount    -ne "$prbsKillerCount" -or `
+    If ($policyCheck[2].LoopCount    -ne "$($prbsKillerCount)" -or `
         $policyCheck[2].pattern      -ne "prbs-killer" -or `
         $policyCheck[2].CpuFilter    -ne "all-cpus" -or `
         $policyCheck[2].MemChunkSize -ne "big-chunk" -or `
         $policyCheck[2].MemSize      -ne "All"
     ){
+        write-screen -type "WARN" -message "`tPRBS-Killer test has a different settings. Recreating the policy"
         return $false
     }    
     # We think everything matches up. 
@@ -111,8 +191,7 @@ function remove-ucsDiagnosticPolicy(){
 function check-DiagPolicy(){
 
     # Check to see if policy already Exists
-    $policyCheck = get-ucsdiagRunPolicy -name $Global:testName -Hierarchy
-
+    $policyCheck = get-ucsdiagRunPolicy -name $Global:testName -Hierarchy -org root
     #Create the policy if it does not already exist
     if ($policyCheck){
         Write-screen -type 'INFO' -message "`t$($Global:testName) Policy Exists"
@@ -151,9 +230,17 @@ Function toolLoadCheck {
 function triggerMemoryTest{
     param()
     $serverList | %{
-        write-screen -type "INFO" -message "`tProcessessing Server for memory testing: $($_)"
-        $removeResult1 = Set-UcsManagedObject -ClassID "diagSrvCtrl" -propertyMap @{runPolicyName="$($Global:testName)";dn="$_"} -confirm:$false -force
-        $removeResult2 = set-UcsManagedObject -ClassID "diagSrvCtrl" -propertyMap @{dn="$_"; adminState="trigger"} -confirm:$false -force
+        $currentServer = $_
+        if ((get-ucsManagedObject -dn $_).count -eq 0){
+            write-screen -type "WARN" -message "`tServer $currentServer couldn't be found in this domain and will not be tested"
+        }
+        else{
+            write-screen -type "INFO" -message "`tProcessessing Server for memory testing: $($_)"
+            #Change the name of the diagnostic test name associated with the specified server 
+            $removeResult1 = Set-UcsManagedObject -ClassID "diagSrvCtrl" -propertyMap @{runPolicyName="$($Global:testName)";dn="$_"} -confirm:$false -force
+            #Start the test
+            $removeResult2 = set-UcsManagedObject -ClassID "diagSrvCtrl" -propertyMap @{dn="$_"; adminState="trigger"} -confirm:$false -force
+        }
     }
 }
 
@@ -163,9 +250,10 @@ function main {
     )
     write-screen -type "INFO" -message "Processing Domain $Domain"
     if ($defaultUCS){
+        #If the variable $defaultUCS exists, we need to close the connection or the script will fail. 
         write-screen "WARN" "We found $($defaultUCS.Ucs) is already connected. We will disconnect that domain before we continue"
         # We already have a connected domain, and we need to fix that. 
-        disconnect-ucs 
+        $hideDisconnectResult = disconnect-ucs 
     }
     if ((toolLoadCheck) -eq $false){
         write-screen -type "INFO" -message "PowerShell modules for UCS are not active. Importing required modules"
@@ -175,6 +263,7 @@ function main {
 
         }
     }
+    #Connect to UCS
     $hideConnectionResult = connect-ucs -Name $domain -Credential $Credentials -ErrorAction:SilentlyContinue
     if ("$?" -eq "False"){
         write-screen -type "FAIL" -message "`tFailed to connect to domain. Script cannot continue.`n$($error[0])"
@@ -184,8 +273,20 @@ function main {
     } 
     check-DiagPolicy
     triggerMemoryTest
+    $hideDisconnectResult = disconnect-ucs
+}
+function validateServers{
+    param()
+    # We look for up to 160 rack mounts, or up to 20 chassis of blades (8 per chassis).
+    $serverList | %{
+        write-verbose "Validating Server: $_"
+        if ($_ -notmatch "sys/chassis-([1-9]|[1][0-9]|[2][0])/blade-([1-8])|/sys/rack-unit-([1-9]|[1-9][0-9]|[1][0-6][0-9])"){
+            write-screen -type "FAIL" -message "Format for servers to modify could not be vaildated. Script cannot continue."
+        }
+    }
 }
 
+validateServers
 $DomainList | %{
     main -domain $_
 }
